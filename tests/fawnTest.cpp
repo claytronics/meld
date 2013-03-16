beccd00843c4e57e4a0b82996cb9481cddf5c581
Dave Campbell <dncswim76@gmail.com>
2013-03-16 17:26:40 -0500

    changed fawnTest

--- tests/fawnTest.cpp
+++ tests/fawnTest.cpp
@@ -37,8 +37,8 @@ int main(int argc, char* argv[])
     mpi::environment env(argc, argv);
     mpi::communicator world;
 
-    string msg1,msg2;
-    string str = "hello";
+    //string msg1,msg2;
+    //string str = "hello";
 
     int sum = 0;
     for (int i = 0; i < L; i++){
@@ -47,15 +47,15 @@ int main(int argc, char* argv[])
         }
         sum = 0;
 
-        world.send(nextProcess(world), SENDNEXT, str);
-        world.irecv(prevProcess(world), SENDNEXT, msg1);
+        world.send(nextProcess(world), SENDNEXT);
+        world.irecv(prevProcess(world), SENDNEXT);
 	
 
-        if (rdvs){
+      /*  if (rdvs){
             world.send(prevProcess(world),WAIT,str);
             world.probe(nextProcess(world),WAIT);
             world.recv(nextProcess(world),WAIT,msg2);
-        }
+        }*/
     }
 
     return 0;
