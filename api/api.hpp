5b517943475ef8fe29580de50be5fd89f1bc5a1d
Xing Zhou <xing@xingzhou.me>
2013-06-17 17:25:55 -0400

    Update API signatures

--- api/api.hpp
+++ api/api.hpp
@@ -1,25 +1,32 @@
+#ifndef API_H_
+#define API_H_
+
 /*
  * Header file for API
  */
 #include "db/tuple.hpp"
 #include "db/node.hpp"
 #include "sched/base.hpp"
+#include <boost/mpi.hpp>
 
 using namespace std;
 
 namespace api {
     /* Type representing the message between interprocess communications */
     typedef uint64_t message_type;
+    extern boost::mpi::communicator world;
 
     /* Given a node destination, compute the process id that the node
      * belongs to, serialize the data for MPI and send the data
      */
-    void send(const db::node::node_id id, db::simple_tuple* tuple);
+    void send_message(const db::node::node_id id, message_type *msg);
+
+    message_type *create_message();
 
     /* Check for any pending messages waiting to be received and add all of
      * the pending messages to the scheduler queue
      */
-    void pool(const sched::base *sched);
+    void poll(const sched::base *sched);
 
     /* Return whether or not the node with id `id` belongs to the current
      * process
@@ -31,3 +38,5 @@ namespace api {
      */
     void init(int argc, char **argv);
 } // namespace api
+
+#endif
