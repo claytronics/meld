2420be6ffbb619e4d983e985acfc382972f7050c
Xing Zhou <xing@xingzhou.me>
2013-06-26 14:48:28 -0400

    Implement token ring algorithm described by Seth

--- api/api.hpp
+++ api/api.hpp
@@ -12,32 +12,34 @@
 #include "vm/all.hpp"
 
 namespace api {
-  /* Type representing the message between interprocess communications */
-  typedef uint64_t message_type;
-  static const size_t MAXLENGTH = 512 / sizeof(message_type);
-  extern boost::mpi::communicator *world;
+    /* Type representing the message between interprocess communications */
+    typedef uint64_t message_type;
+    static const size_t MAXLENGTH = 512 / sizeof(message_type);
+    extern boost::mpi::communicator *world;
+    extern bool reset_token;
 
-  /* Given a node destination, compute the process id that the node
-   * belongs to, serialize the data for MPI and send the data
-   */
-  extern void send_message(const db::node::node_id id, const db::simple_tuple *stpl);
 
-  extern message_type *create_message(const db::simple_tuple *tuple);
+    /* Given a node destination, compute the process id that the node
+     * belongs to, serialize the data for MPI and send the data
+     */
+    extern void send_message(const db::node::node_id id, const db::simple_tuple *stpl);
 
-  /* Check for any pending messages waiting to be received and add all of
-   * the pending messages to the scheduler queue
-   */
-  extern bool poll(sched::base *sched, vm::all *all);
+    extern message_type *create_message(const db::simple_tuple *tuple);
 
-  /* Return whether or not the node with id `id` belongs to the current
-   * process
-   */
-  extern bool on_current_process(const db::node::node_id id);
+    /* Check for any pending messages waiting to be received and add all of
+     * the pending messages to the scheduler queue
+     */
+    extern bool poll(sched::base *sched, vm::all *all);
 
-  /*
-   * Initialize the API layer
-   */
-  extern void init(int argc, char **argv);
+    /* Return whether or not the node with id `id` belongs to the current
+     * process
+     */
+    extern bool on_current_process(const db::node::node_id id);
+
+    /*
+     * Initialize the API layer
+     */
+    extern void init(int argc, char **argv);
 } // namespace api
 
 #endif
--- api/mpi.cpp
+++ api/mpi.cpp
@@ -14,6 +14,7 @@ using namespace std;
 namespace api {
     boost::mpi::communicator *world;
     int world_sum;
+    bool reset_token;
 
     /* Send data to the dest */
     void send_message(const db::node::node_id id,
@@ -35,17 +36,21 @@ namespace api {
 	 */
 	boost::optional<boost::mpi::status> status_opt;
 	boost::mpi::status status;
+	bool received_self = false;
 	bool consume_token = false;
-	int token = -1;
+	int token;
+	int dest = (world->rank() + 1) % world->size();
 
 	/* TODO Token_tag needs to be the same across process */
-	int TOKEN_TAG = all->DATABASE->nodes_total;
+	int TOKEN_TAG = all->DATABASE->max_id() + world->size();
+
+	world->isend(dest, TOKEN_TAG, world->rank());
 
 	while ((status_opt = world->iprobe(boost::mpi::any_source,
-					       boost::mpi::any_tag))) {
+					   boost::mpi::any_tag))) {
 	    status = status_opt.get();
 
-	    if (status.tag() != TOKEN_TAG) {
+	    if (status.tag() < TOKEN_TAG) {
 		/* a tuple message that needs to be processed by scheduler */
 		consume_token = true;
 
@@ -63,37 +68,57 @@ namespace api {
 				    stpl, 0);
 	    } else {
 		world->irecv(boost::mpi::any_source, TOKEN_TAG, token);
-		assert(token >= 0 && token <= 2 * world_sum);
-	    }
-	}
+		assert(token >= 0);
 
-	if (consume_token || sched->get_work())
-	    return true;
-	else {
-	    int dest = (world->rank()+1) % world->size();
-
-	    if(token >= world_sum) {
-		/* token has traveled around the ring at least twice,
-		 * should be safe to terminate the process */
-
-		world->isend(dest, TOKEN_TAG, token);
-		cout << "Proc: " << world->rank() << " end token" << endl;
-		return false;
-	    } else {
-		if (token >= 0) {
-		    /* Accumulate into token */
-		    world->isend(dest, TOKEN_TAG, world->rank() + token);
-		    cout << "Proc: " << world->rank() << " acc token" << endl;
+		if (token != world->rank()) {
+		    world->isend(dest, TOKEN_TAG, token);
 		} else {
-		    /* start off a new token */
-		    world->isend(dest, TOKEN_TAG, world->rank());
-		    cout << "Proc: " << world->rank() << " new token" << endl;
+		    received_self = true;
 		}
-
-		boost::this_thread::sleep(boost::posix_time::milliseconds(10));
-		return true;
 	    }
+	    break;
 	}
+
+	if (received_self && !consume_token) {
+	    /* Terminate */
+	    return false;
+	} else {
+	    return true;
+	}
+	// if (consume_token || sched->get_work())
+	//     return true;
+	// else {
+	//     int dest = (world->rank()+1) % world->size();
+
+	//     if (reset_token) {
+	// 	/* start off a new token */
+	// 	world->isend(dest, TOKEN_TAG, world->rank());
+	// 	cout << "Proc: " << world->rank() << " new token" << endl;
+	// 	api::reset_token = false;
+	//     } else {
+	// 	if(token >= 2 * world_sum) {
+	// 	    /* token has traveled around the ring at least twice,
+	// 	     * should be safe to terminate the process */
+
+	// 	    world->isend(dest, TOKEN_TAG, token);
+	// 	    cout << "Proc: " << world->rank() << " end token" << endl;
+	// 	    assert(!sched->get_work());
+	// 	    return false;
+	// 	} else {
+	// 	    if (token < 0) {
+	// 		world->isend(dest, TOKEN_TAG, world->rank());
+	// 	    } else {
+	// 		/* Accumulate into token */
+	// 		world->isend(dest, TOKEN_TAG, world->rank() + token);
+	// 		cout << "Proc: " << world->rank() << " acc token" << endl;
+	// 	    }
+	// 	}
+
+	//     }
+
+	//     boost::this_thread::sleep(boost::posix_time::milliseconds(10));
+	//     return true;
+	// }
     }
 
     message_type *create_message(const db::simple_tuple *tuple) {
@@ -111,6 +136,7 @@ namespace api {
 
     void init(int argc, char **argv) {
 	world_sum = (world->size() - 1) * (world->size()) / 2;
+	reset_token = true;
     }
 
 } /* namespace api */
