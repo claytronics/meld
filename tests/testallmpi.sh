2228c0b98cd3c147a97b0bfcded2878613697e82
Seth Goldstein <seth@flashgroup.com>
2013-10-11 14:41:00 -0400

    add check for target

--- tests/testallmpi.sh
+++ tests/testallmpi.sh
@@ -2,5 +2,10 @@
 if [ ${MELDCL:-unset} == unset ]; then
   echo "You need to set MELDCL to point to directory with meld compiler"
 fi
+../meld -a mpi
+if [ $? != 0 ]; then
+    echo "vm is not compiled to use mpi"
+    exit 1
+fi
 for i in progs/*.meld; do make -f test.make FILE=$i TARGET=mpi NODES=1; done
 exit
