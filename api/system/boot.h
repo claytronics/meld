56b9e30ea902529751b70465d3cf80c7f264f053
dcampbel <dncswim76@gmail.com>
2013-11-14 15:03:06 -0500

    added hardware API

---
+++ api/system/boot.h
@@ -0,0 +1,8 @@
+# 1 "/home/dcampbel/Research/build-romain/src-bobby/system/boot.bbh"
+#ifndef __BOOT_H__
+#define __BOOT_H__
+//---------- BOOT.H------------------
+
+void jumpToBootSection(void);
+
+#endif
---
+++ api/system/debug.c
@@ -0,0 +1,8 @@
+# 1 "/home/dcampbel/Research/build-romain/src-bobby/system/debug.bb"
+#include "debug.h"
+#include "../hw-api/hwDebug.h"
+
+void initDebug()
+{
+  initHWDebug();
+}
